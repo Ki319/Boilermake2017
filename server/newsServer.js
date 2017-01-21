ae464e0eba9c7516ce4c29ad4f125f72045d1556
Maxattax97 <maxattax97@gmail.com>
2017-01-20 22:26:20 -0500

    Fix parentheses typo

--- server/newsServer.js
+++ server/newsServer.js
@@ -1,6 +1,6 @@
 var http = require("http");
 
-http.createServer(function (req, res)) {
+http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Hello world!\n");
 }).listen(9090, "127.0.0.1");
