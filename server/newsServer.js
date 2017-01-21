de6eefa5b2f5929fb96ca8c1b5842b09f11503d9
Maxattax97 <maxattax97@gmail.com>
2017-01-21 00:26:03 -0500

    Re-enable normal logic

--- server/newsServer.js
+++ server/newsServer.js
@@ -1,9 +1,11 @@
 var http = require("http");
 
 http.createServer(function (req, res) {
-    //var body = [];
+    var body = [];
 
-    /*req.on("data", function (buff) {
+    console.log("Connection established.");
+
+    req.on("data", function (buff) {
         body.push(buff);
     }).on("end", function() {
         body = Buffer.concat(body).toString();
@@ -14,14 +16,7 @@ http.createServer(function (req, res) {
         body = [];
     }).on("error", function(err) {
         console.error(err.stack);
-    });*/
-
-    console.log("Connection established.");
-
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Connection established.\n");
-
-
+    });
 }).listen(9090, "0.0.0.0");
 
 console.log("Server running on port 9090.");
